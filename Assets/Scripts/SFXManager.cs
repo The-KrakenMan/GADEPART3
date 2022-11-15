5b85f8b894ff8ea0592d4a76665d9cde744ee6d5
DuncanSmit01 <88308215+DuncanSmit01@users.noreply.github.com>
2022-11-15 13:38:57 +0200

    Hashtables

--- Assets/Scripts/SFXManager.cs
+++ Assets/Scripts/SFXManager.cs
@@ -4,10 +4,28 @@ using UnityEngine;
 
 public class SFXManager : MonoBehaviour
 {
+    public static SFXManager instance;
+
+    public Hashtable hasht1;
+    public Hashtable hasht2;
+    public AudioSource pCrash;
+    public AudioSource pWin;
+    public AudioSource pGameover;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        hasht1 = new Hashtable();
+        hasht1.Add(1, "crash");
+        hasht1.Add(2, "Win");
+        hasht1.Add(3, "gameOver");
+
+        Dictionary<int, AudioSource> audioSource = new Dictionary<int, AudioSource>();
+        audioSource.Add(1, pCrash);
+        audioSource.Add(2, pWin);
+        audioSource.Add(3, pGameover);
+
+        hasht2 = new Hashtable(audioSource);
     }
 
     // Update is called once per frame
